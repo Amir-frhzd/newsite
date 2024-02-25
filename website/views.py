674f3f5c0a6012d058e7ac3675d3bdd7a03dfec8
xzx <goodstar40@yahoo.com>
2024-02-25 11:51:56 +0330

    static file and templates added.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index_view(request):
+    return render(request,'website/index.html')
+
+def about_view(request):
+    return render(request,'website/about.html')
+
+
+def contact_view(request):
+    return render(request,'website/contact.html')
+
+def work_view(request):
+    return render(request,'website/work.html')
+
+def blog_view(request):
+    return render(request,'website/blog.html')
+
+
+
 # Create your views here.
